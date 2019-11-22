0bc63fd354e3f6bc9753232452521ee228976a54
Kim Dahee <ekekekgml@naver.com>
2019-11-22 21:41:54 +0900

    [Initial] Spring boot board

---
+++ src/main/java/com/example/board/board/mapper/BoardMapper.java
@@ -0,0 +1,23 @@
+package com.example.board.board.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.board.board.dto.BoardDto;
+
+
+@Mapper
+public interface BoardMapper {
+    List<BoardDto> selectBoardList() throws Exception;
+    void insertBoard(BoardDto board) throws Exception;
+
+    void updateHitCount(int boardIdx) throws Exception;
+
+    BoardDto selectBoardDetail(int boardIdx) throws Exception;
+
+    void updateBoard(BoardDto board) throws Exception;
+
+    void deleteBoard(int boardIdx) throws Exception;
+}
+
---
+++ src/main/java/com/example/board/board/service/BoardService.java
@@ -0,0 +1,17 @@
+package com.example.board.board.service;
+
+import com.example.board.board.dto.BoardDto;
+import com.example.board.board.mapper.BoardMapper;
+import java.util.List;
+
+public interface BoardService {
+    List<BoardDto> selectBoardList() throws Exception;
+
+    void insertBoard(BoardDto board) throws Exception;
+
+    BoardDto selectBoardDetail(int boardIdx) throws Exception;
+
+    void updateBoard(BoardDto board) throws Exception;
+
+    void deleteBoard(int boardIdx) throws Exception;
+}
